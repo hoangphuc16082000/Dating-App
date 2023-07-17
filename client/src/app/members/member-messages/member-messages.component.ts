bb51fe58e5e0148b4776c0cb1792bec7673b255e
Hoang phuc <hoangphuc16082000@gmail.com>
2023-07-17 10:46:47 +0700

    section 19(don't deploy fly)

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -18,6 +18,7 @@ export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) {}
 
@@ -25,10 +26,12 @@ export class MemberMessagesComponent {
 
   sendMessage() {
     if (!this.username) return;
+    this.loading = true;
     this.messageService
       .sendMessage(this.username, this.messageContent)
       .then(() => {
         this.messageForm?.reset();
-      });
+      })
+      .finally(() => (this.loading = false));
   }
 }
